ce7bfff6a9ba71dc8eb4bd5be9166efd4466a3e1
Satyajit <sahoosatyajit2801@gmail.com>
2023-09-30 23:57:20 +0530

    added login page and it's functionality

--- crm_project/crm_app/views.py
+++ crm_project/crm_app/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def index(request):
     return render(request, 'base.html')
 
 def home(request):
+
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have login successfully")
+            return redirect('home')
+        else:
+            messages.success(request, "Oops...Try Again!!!")
+            return redirect('home')
+    else:
+        return render(request,'home.html')
+    
     return render(request, 'home.html')
