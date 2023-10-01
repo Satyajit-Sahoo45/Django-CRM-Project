124ac32763b409964255b4bb1512cd0bd0d7a413
Satyajit <sahoosatyajit2801@gmail.com>
2023-10-01 11:28:14 +0530

    added record model

--- crm_project/crm_app/models.py
+++ crm_project/crm_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Record(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+
+    class Meta:
+        db_table = "records"
+
