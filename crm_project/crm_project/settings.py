9df876d1266da43d7f8e29b6d10ba7578469d8e3
Satyajit <sahoosatyajit2801@gmail.com>
2023-10-02 19:51:16 +0530

    added import-export func

--- crm_project/crm_project/settings.py
+++ crm_project/crm_project/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crm_app'
+    'crm_app',
+    'import_export',
 ]
 
 MIDDLEWARE = [
